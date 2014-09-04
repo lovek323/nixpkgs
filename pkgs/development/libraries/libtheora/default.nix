ed236edc8fff4233eafd7a8513c38fc4b7c10137
Jason "Don" O'Conal <lovek323@gmail.com>
2014-09-04 17:39:59 +1000

    libtheora: fix build on darwin

--- pkgs/development/libraries/libtheora/default.nix
+++ pkgs/development/libraries/libtheora/default.nix
@@ -1,4 +1,5 @@
-{stdenv, fetchurl, libogg, libvorbis, tremor, autoconf, automake, libtool}:
+{ stdenv, fetchurl, libogg, libvorbis, tremor, autoconf, automake, libtool
+, pkgconfig }:
 
 stdenv.mkDerivation ({
   name = "libtheora-1.1.1";
@@ -7,10 +8,22 @@ stdenv.mkDerivation ({
     sha256 = "0swiaj8987n995rc7hw0asvpwhhzpjiws8kr3s6r44bqqib2k5a0";
   };
 
-  propagatedBuildInputs = [libogg libvorbis];
+  buildInputs = [ pkgconfig ];
+
+  propagatedBuildInputs = [ libogg libvorbis ];
+
+  postConfigure = stdenv.lib.optionalString stdenv.isDarwin ''
+    sed -i 's/-fforce-addr//' lib/Makefile
+    sed -i 's/-fforce-addr//' doc/spec/Makefile
+  '';
+
+  configureFlags = stdenv.lib.optionals stdenv.isDarwin [
+    "--disable-vorbistest"
+    "--disable-examples"
+  ];
 
   crossAttrs = {
-    propagatedBuildInputs = [libogg.crossDrv tremor.crossDrv];
+    propagatedBuildInputs = [ libogg.crossDrv tremor.crossDrv ];
     configureFlags = "--disable-examples";
   };
 }
@@ -18,6 +31,6 @@ stdenv.mkDerivation ({
 # It has an old config.guess that doesn't know the mips64el.
 // stdenv.lib.optionalAttrs (stdenv.system == "mips64el-linux")
 {
-  propagatedBuildInputs = [libogg libvorbis autoconf automake libtool];
+  propagatedBuildInputs = [ libogg libvorbis autoconf automake libtool ];
   preConfigure = "rm config.guess; sh autogen.sh";
 })
